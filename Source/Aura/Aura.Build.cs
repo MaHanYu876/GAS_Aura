56b6a89e4a1cfddaf2500b174994957724b10f8a
mhy <2977843894@qq.com>
2026-01-16 16:25:47 +0800

    AbilitySystemComponent and attributeSet

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -7,17 +7,21 @@ public class Aura : ModuleRules
 	public Aura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			"GameplayAbilities", // GAS 核心逻辑
+			"GameplayTags",      // GAS 状态标签
+			"GameplayTasks"      // 解决 LNK2001 报错的关键模块 });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+			// Uncomment if you are using Slate UI
+			// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+			// Uncomment if you are using online features
+			// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+			// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		});
 	}
 }
