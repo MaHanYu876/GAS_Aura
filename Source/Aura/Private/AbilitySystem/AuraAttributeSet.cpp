70db5d64fcb077150b14a8133d9564be0bd65187
mhy <2977843894@qq.com>
2026-01-17 17:07:30 +0800

    define GameplayAttribute by AttributeSet

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,41 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "Net/UnrealNetwork.h"
+#include "AbilitySystemComponent.h"
+UAuraAttributeSet::UAuraAttributeSet()
+{
 
+}
+
+//注册同步规则
+//REPNOTIFY_Always表示每次属性变化都会触发OnRep函数
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mona, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMona, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mona(const FGameplayAttributeData& OldMona) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mona, OldMona);
+}
+
+void UAuraAttributeSet::OnRep_MaxMona(const FGameplayAttributeData& OldMaxMona) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMona, OldMaxMona);
+}
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -6,6 +6,8 @@
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
 
+struct FGameplayAttributeData;
+
 /**
  * 
  */
@@ -13,5 +15,36 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UAuraAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	
+	//定义gameplay属性
+	//ReplicatedUsing = OnRep_Health 指定属性变化时调用的函数
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mona, Category = "Vital Attributes")
+	FGameplayAttributeData Mona;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMona, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMona;
+
+	//同步属性变化的函数（更新本地状态）
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mona(const FGameplayAttributeData& OldMona) const;
+
+	UFUNCTION()
+	void OnRep_MaxMona(const FGameplayAttributeData& OldMaxMona) const;
+
 };
