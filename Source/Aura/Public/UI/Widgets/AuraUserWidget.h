0114a55068c7a6bf5c87178adf755a48375fc71b
mhy <2977843894@qq.com>
2026-01-19 10:48:29 +0800

    AuraUserWidget and WidgetController

---
+++ Source/Aura/Public/UI/Widgets/AuraUserWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+
+	//持有WidgetController的引用，不直接访问GAS的数据，而是通过Controller间接获取
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+protected:
+	//当c++层通过SetWidgetController设置了WidgetController后，会调用这个事件通知蓝图层绑定数据到UI
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+
+};
